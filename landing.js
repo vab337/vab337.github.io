eec6072129131c3698e1285a39d389c0e0ba2391
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-02-05 20:14:29 +0400

    updated files

---
+++ landing.js
@@ -0,0 +1,10 @@
+var doors = document.querySelector('img');
+
+doors.onclick = function() {
+      doors.setAttribute ('src','images/doors.gif');
+      setTimeout(changePage, 3500);
+}
+
+ function changePage() {
+     window.location.href = "index.html";
+   }
