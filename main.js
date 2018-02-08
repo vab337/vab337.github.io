036ee035f011e757f19fdc2d774523f1212250c0
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-02-08 11:48:31 +0400

    animation added

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+window.onscroll = function() { RevealAbout()};
 
+function RevealAbout() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("about").className = "slideIn";
+    } else {
+        document.getElementById("about").className = "about";
+    }
+}
