4b55f3673fe3a79d98a7fd1ed940c82350feb64f
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-02-11 17:24:49 +0400

    complete ccontent

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-window.onscroll = function() { RevealAbout()};
+let UpperBody = document.getElementById("upper-body")
+let UpArrow = document.getElementById('up-arrow')
+let About = document.getElementById("about")
+let Team = document.getElementById('team')
 
-function RevealAbout() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("about").className = "slideIn";
-    } else {
-        document.getElementById("about").className = "about";
-    }
+// window.onscroll = () => {
+//   if (document.body.scrollTop > 200) {
+//         document.getElementById("about").className = "slideIn"
+//         UpperBody.style.opacity = 0
+//         UpArrow.style.opacity = 1
+//     } else {
+//         document.getElementById("about").className = "about"
+//         UpperBody.style.opacity = 1
+//     }
+// }
+
+UpArrow.onclick = () => {
+  window.scrollTo(0, 0)
+  UpperBody.style.opacity = 1
+}
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 200 && document.body.scrollTop < 600) {
+        About.style.opacity = 1
+        About.style.paddingLeft = '2%'
+        UpperBody.style.opacity = 0
+        UpArrow.style.opacity = 1
+    } else
+      {if (document.body.scrollTop > 650) {
+      Team.style.opacity = 1
+      } else {
+      UpperBody.style.opacity = 1
+      }
+}
 }
