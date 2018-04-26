5da776dde7dd63a496a3261c970a5e1772ba8894
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-04-26 12:54:50 +0400

    vve

--- Codingfuntimes/script.js
+++ Codingfuntimes/script.js
@@ -1,13 +1,15 @@
 let totoro = document.getElementById('totoro')
 let introVideo = document.getElementById('introVideo')
 
-totoro.onlick = () => {
-  let style = document.getElementById('stylesheet')
-  stylesheet.setAttribute("href", "style2.css")
-}
-
 totoro.addEventListener("click", displayvideo)
 
 function displayvideo() {
   introVideo.style.visibility = "visible"
+}
+
+totoro.addEventListener("click",changecolor)
+
+function changecolor () {
+  let style = document.getElementById('stylesheet')
+  stylesheet.setAttribute("href", "style2.css")
 }
