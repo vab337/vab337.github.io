bd46560cc945dacbe109e25ff715cfb060cb29fb
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-05-07 18:10:55 +0400

    iintro

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let vango = document.getElementById('vango')
 
 vango.onclick = () => {
-  let style = document.getElementById('stylesheet')
+  let stylesheet = document.getElementById('stylesheet')
   stylesheet.setAttribute("href", "style.css")
+  stylesheet.style.transition  = "all 2s"
 }
