945b3de1af7335417fa9c335c565b59b5e068464
Bui Van Anh <buivananh@Buis-MacBook-Pro.local>
2018-05-08 12:13:49 +0400

    ww

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let vango = document.getElementById('vango')
+let mff = document.getElementById('mff')
+let sound = document.getElementById('sound')
+let comics = document.getElementById('comics')
+let video = document.getElementById('video')
+let animation = document.getElementById('animation')
 
-// vango.onclick = () => {
-//   let stylesheet = document.getElementById('stylesheet')
-//   stylesheet.setAttribute("href", "style.css")
-//   stylesheet.style.transition  = "all 2s"
-// }
+mff.addEventListener("mouseover", mffover)
+sound.addEventListener("mouseover", soundover)
+comics.addEventListener("mouseover", comicsover)
+video.addEventListener("mouseover", videoover)
+animation.addEventListener("mouseover", animationover)
+
+
+mff.addEventListener("mouseleave", leave)
+sound.addEventListener("mouseleave", leave)
+comics.addEventListener("mouseleave", leave)
+video.addEventListener("mouseleave", leave)
+animation.addEventListener("mouseleave", leave)
